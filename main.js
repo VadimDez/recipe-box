5d797a60c0c51675a84774d77be236e31ceb264b
Vadym Yatsyuk <vasp3d@gmail.com>
2016-01-31 08:36:32 +0100

    used provider from lib

--- main.js
+++ main.js
@@ -5,29 +5,14 @@ import Main from './components/Main';
 import React from 'react';
 import {render} from 'react-dom';
 import store from './store';
-
-class Provider extends React.Component {
-  getChildContext() {
-    return {
-      store: this.props.store
-    }
-  }
-
-  render() {
-    return this.props.children;
-  }
-}
-
-Provider.childContextTypes = {
-  store: React.PropTypes.object
-};
+import { Provider } from 'react-redux';
 
 const renderer = () => {
   render(
     <Provider store={store}>
       <Main />
-    </Provider>
-    , document.getElementById('app'));
+    </Provider>,
+    document.getElementById('app'));
 };
 
 store.subscribe(renderer);
